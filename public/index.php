1ff9645352f6710573881deb97e726f24cf4319a
JustGoJaeHo <justgojaeho@gmail.com>
2025-02-01 19:43:08 +0900

    Chapter 08 객체와 클래스

--- public/index.php
+++ public/index.php
@@ -1,11 +1,44 @@
 <?php
-    $title = '인터넷 유머 세상';
+    function loadTemplate($templateFileName, $variables = []) {
+        extract($variables);
 
-    ob_start();
+        ob_start();
 
-    include __DIR__ . '/../templates/home.html.php';
+        include __DIR__ . '/../templates/' . $templateFileName;
 
-    $output = ob_get_clean();
+        return ob_get_clean();
+    }
+
+    try {
+        include __DIR__ . '/../includes/DatabaseConnection.php';
+        include __DIR__ . '/../classes/DatabaseTable.php';
+        include __DIR__ . '/../controllers/JokeController.php';
+
+        $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
+        $authorsTable = new DatabaseTable($pdo, 'author', 'id');
+
+        $jokeController = new JokeController($jokesTable, $authorsTable);
+
+        $action = $_GET['action'] ?? 'home';
+
+        $page = $jokeController->$action();
+
+        $title = $page['title'];
+
+        if (isset($page['variables'])) {
+            $output = loadTemplate($page['template'], $page['variables']);
+        } else {
+            $output = loadTemplate($page['template']);
+        }
+
+    } catch (PDOException $e) {
+        $title = '오류가 발생했습니다';
+
+        $output = '데이터베이스 오류: ' .
+        $e->getMEssage() . ', 위치: ' .
+        $e->getFile() . ':' .
+        $e->getLine();
+    }
 
     include __DIR__ . '/../templates/layout.html.php';
 ?>
